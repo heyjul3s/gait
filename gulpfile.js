33bf13798ee6ed8365615b9948f06ef2d77e69e6
Julian Low <oijulijuli@gmail.com>
2016-07-22 18:12:55 +1000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+(function(){
+    'use strict';
+
+    var gulp          = require('gulp'),
+        plumber       = require('gulp-plumber'),
+        sourcemaps    = require('gulp-sourcemaps'),
+        pug           = require('gulp-pug'),
+        stylus        = require('gulp-stylus'),
+        nib           = require('nib'),
+        jeet          = require('jeet'),
+        rupture       = require('rupture'),
+        typographic   = require('typographic'),
+        autoprefixer        = require('gulp-autoprefixer');
+
+    var paths = {
+        src : {
+            pug: 'index.pug',
+            stylus: 'styl/**/*.styl'
+        },
+        dest : {
+            html: './',
+            css: './dist/'
+        }
+    };
+
+
+    gulp.task('pug', function(){
+        return gulp.src( paths.src.pug )
+            .pipe( plumber() )
+            .pipe( pug({
+                pretty: true
+            }))
+            .pipe( gulp.dest( paths.dest.html ) );
+    });
+
+
+    gulp.task('autoprefixer', function(){
+        return gulp.src( paths.src.stylus )
+            .pipe( plumber() )
+            .pipe( autoprefixer([
+                'last 2 versions',
+                '> 1%',
+                'ie 10'
+            ]))
+            .pipe( gulp.dest(paths.dest.css) );
+    });
+
+
+    gulp.task('stylus', function(){
+        return gulp.src( paths.src.stylus )
+            .pipe( plumber() )
+            .pipe( sourcemaps.init() )
+            .pipe( stylus({
+
+            paths: [
+                'node_modules',
+                paths.src.stylus
+            ],
+
+            import: [
+                // 'jeet/stylus/jeet',
+                'nib',
+                'rupture/rupture',
+            ],
+
+            use: [
+                nib(),
+                typographic(),
+                rupture(),
+                jeet()
+            ],
+
+            'include css': true
+
+        }))
+        .pipe( autoprefixer() )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest( paths.dest.css ) );
+    });
+
+
+    gulp.task('watch', function() {
+        gulp.watch( paths.src.pug, [
+            'pug',
+        ]);
+
+        gulp.watch( paths.src.stylus, [
+            'stylus',
+        ]);
+    });
+
+
+    gulp.task('default', [
+        'watch'
+    ]);
+}());
